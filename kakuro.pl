0eb4f08951bd9b5d0110caf9463316889e5e328c
Luís Fonseca <luis.h.fonseca@tecnico.ulisboa.pt>
2021-04-28 23:39:26 +0100

    Completed predicado permutacoes_soma

--- kakuro.pl
+++ kakuro.pl
@@ -8,4 +8,18 @@
 % soma eh Soma.
 %-------------------------------------------------------------------------------
 combinacoes_soma(N, Els, Soma, Combs) :-
-    findall(Comb, (combinacao(N, Els, Comb), sum_list(Comb, Soma)), Combs).
+    setof(Comb, (combinacao(N, Els, Comb), sum_list(Comb, Soma)), Combs).
+
+%-------------------------------------------------------------------------------
+%               permutacoes_soma(N, Els, Soma, Perms)
+% permutacoes_soma(N, Els, Soma, Perms), em que N eh um inteiro, Els eh uma
+% lista de inteiros, e Soma eh um inteiro, significa que Perms eh a lista
+% ordenada cujos elementos sao as permutacoes das combinacoes N a N, dos
+% elementos de Els cuja soma eh Soma.
+%-------------------------------------------------------------------------------
+permutacoes_soma(N, Els, Soma, Perms) :-
+    combinacoes_soma(N, Els, Soma, Combs),
+    findall(Perm, (member(Comb, Combs), permutation(Comb, Perm)), Unsorted),
+    sort(Unsorted, Perms).
+
+
