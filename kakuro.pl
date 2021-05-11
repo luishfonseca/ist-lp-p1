24d48805efe50ce66176470b433335006323921f
Luís Fonseca <luis.h.fonseca@tecnico.ulisboa.pt>
2021-05-11 23:38:03 +0100

    import common code

--- kakuro.pl
+++ kakuro.pl
@@ -1,3 +1,4 @@
+:- [codigo_comum].
 %-------------------------------------------------------------------------------
 %               combinacoes_soma(N, Els, Soma, Combs)
 % combinacoes_soma(N, Els, Soma, Combs), em que N eh um inteiro, Els eh uma
